375a303bf95a428a637a77ce399e953e99f99887
vikas <vikask@mirakitech.com>
2026-02-02 15:45:33 +0530

    ready for deplyment

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 // const express = require('express');
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
 const app = express();
+const  __dirname = path.resolve();
 
 dotenv.config();
 // app.listen(3000, () => console.log("server is running"));
@@ -18,3 +20,12 @@ app.listen(3000, ()=>{
 
 app.use('/api/auth', authRouter);
 app.use('/api/message', messageRouter);
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+    // if not user try to enter any url other than /api/auth or /api/message it will redirect to index.html of frontend
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
